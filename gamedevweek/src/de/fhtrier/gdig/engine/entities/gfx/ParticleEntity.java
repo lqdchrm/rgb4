7b624da10a4fdc61d092be271722df1cb98228d6
Paul Schiffgens <schiffgp@fh-trier.de>
2010-09-08 18:26:17 +0200

    missing files added

---
+++ gamedevweek/src/de/fhtrier/gdig/engine/entities/gfx/ParticleEntity.java
@@ -0,0 +1,26 @@
+package de.fhtrier.gdig.engine.entities.gfx;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.particles.ParticleSystem;
+
+import de.fhtrier.gdig.engine.management.AssetMgr;
+
+public class ParticleEntity extends AssetEntity {
+
+	public ParticleEntity (int id, int assetId, AssetMgr assets) {
+		super(id, assetId, assets);
+	}
+
+	@Override
+	public void renderImpl(Graphics graphicContext, Image frameBuffer) {
+		if (isVisible()) {
+			ParticleSystem system = Assets().getParticleSystem(getAssetId());
+			// TODO do update somewhere else
+			system.update(10);
+			system.render();
+		}
+		super.renderImpl(graphicContext, frameBuffer);
+	}
+	
+}
