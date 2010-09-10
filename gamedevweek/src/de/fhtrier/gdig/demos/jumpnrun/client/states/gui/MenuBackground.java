602293cb4efe3dbbe4a864670b1c30bef2570c97
wockenff <wockenff@fh-trier.de>
2010-09-10 21:25:05 +0200

    Shader!

--- gamedevweek/src/de/fhtrier/gdig/demos/jumpnrun/client/states/gui/MenuBackground.java
+++ gamedevweek/src/de/fhtrier/gdig/demos/jumpnrun/client/states/gui/MenuBackground.java
@@ -8,6 +8,7 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 import de.fhtrier.gdig.demos.jumpnrun.identifiers.Assets;
+import de.fhtrier.gdig.demos.jumpnrun.identifiers.Settings;
 import de.fhtrier.gdig.engine.graphics.shader.Shader;
 import de.fhtrier.gdig.engine.management.AssetMgr;
 
@@ -50,12 +51,12 @@ public class MenuBackground {
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 	throws SlickException {
 		
-		if (++counter > 60)
+		if (++counter > 120)
 		{
 			counter = 0;
-			colr = (float)Math.random()/5-0.1f;
-			colg = (float)Math.random()/5-0.1f;
-			colb = (float)Math.random()/5-0.1f;
+			colr = (float)Math.random()/10-0.05f;
+			colg = (float)Math.random()/10-0.05f;
+			colb = (float)Math.random()/10-0.05f;
 		}
 		
 		col.r = clamp(col.r+colr, 0, 2);
